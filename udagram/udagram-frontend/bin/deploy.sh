8a165ea5a0ac1baa5979a874f43ec64a132781ff
duydv <danhhieu301098@gmail.com>
2023-06-21 00:35:42 +0700

    git push

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://mybucket011678701733/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mybucket011678701733/
+aws s3 cp --recursive --acl public-read ./www s3://mybucket0116787017333/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mybucket0116787017333/
